93d3a5d1468564f329d3d1355af6cb62f8c6092d
giuvallente <giuliagv@al.insper.edu.br>
2024-09-18 08:45:30 -0300

    Spring

---
+++ avIntermediaria/avIntermediaria/src/main/java/br/insper/avIntermediaria/AvIntermediariaApplication.java
@@ -0,0 +1,13 @@
+package br.insper.avIntermediaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvIntermediariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvIntermediariaApplication.class, args);
+	}
+
+}
