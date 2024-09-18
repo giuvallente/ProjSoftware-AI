278034b3d16254ee047e8f455f0c788b9681bc1f
giuvallente <giuliagv@al.insper.edu.br>
2024-09-18 08:45:30 -0300

    Spring

---
+++ avIntermediaria/avIntermediaria/src/test/java/br/insper/avIntermediaria/AvIntermediariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.avIntermediaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvIntermediariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
